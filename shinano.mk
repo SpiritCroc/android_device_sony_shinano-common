9470986481bf4e2c4b3b593f48174f71b376e1c3
nailyk-fr <nailyk_git@nailyk.fr>
2017-04-12 02:01:14 +0200

    shinano-common: build: Update nfc dependencies
    Change-Id: I2b357321b3fe0c4da0ea89461182fb81dcea07ea

--- shinano.mk
+++ shinano.mk
@@ -130,14 +130,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     NfcNci \
     Tag \
-    com.android.nfc_extras \
     nfc_nci.pn54x.default
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/rootdir/system/etc/nfcee_access.xml:system/etc/nfcee_access.xml \
-    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
-    frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml
 
 # Off mode charger
 PRODUCT_PACKAGES += \
